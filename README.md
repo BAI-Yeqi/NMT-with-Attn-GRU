517280c295c3bf72729b65842600605819bb937e
BAI-Yeqi <981011byq@gmail.com>
2021-03-20 10:39:29 +0000

    finish dataset.py initialize model.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NMT-with-Attn-GRU
 Neural Machine Translation with Attentive GRU
+
+## Dataset
+Download data (https://www.manythings.org/anki/):
+```
+wget https://download.pytorch.org/tutorial/data.zip
+unzip data.zip
+```
--- dataset.py
+++ dataset.py
@@ -13,16 +13,19 @@ import string
 import re
 import random
 
-import torch
-import torch.nn as nn
-from torch import optim
-import torch.nn.functional as F
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 SOS_token = 0
 EOS_token = 1
+MAX_LENGTH = 10
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s ",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
 
 
 class Lang:
@@ -46,3 +49,74 @@ class Lang:
         else:
             self.word2count[word] += 1
 
+
+def unicodeToAscii(s):
+    '''
+    Turn a Unicode string to plain ASCII
+    '''
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+def normalizeString(s):
+    '''
+    Lowercase, trim, and remove non-letter characters
+    '''
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH and \
+        p[1].startswith(eng_prefixes)
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+def prepareData(lang1, lang2, reverse=False):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    pairs = filterPairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+
+if __name__ == '__main__':
+    input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
+    print(random.choice(pairs))
