fdc43b4038a48acaca7567d8e302e02257b630af
BAI-Yeqi <981011byq@gmail.com>
2021-03-26 08:37:55 +0000

    beam search v1

--- evaluator.py
+++ evaluator.py
@@ -9,6 +9,7 @@ email: yeqi001@ntu.edu.sg
 import torch
 import os
 import random
+from copy import deepcopy
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -47,9 +48,7 @@ def evaluate(encoder, decoder, sentence,
             encoder_outputs[ei] += encoder_output[0, 0]
 
         decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
-
         decoder_hidden = encoder_hidden
-
         decoded_words = []
         decoder_attentions = torch.zeros(max_length, max_length)
 
@@ -66,25 +65,90 @@ def evaluate(encoder, decoder, sentence,
                 else:
                     decoded_words.append(output_lang.index2word[topi.item()])
                 decoder_input = topi.squeeze().detach()
+            return decoded_words, decoder_attentions[:di + 1]
         else:
             # Beam search
-            word_id_seqs = [[]] * beam_size
-            previous_hidden_states = [encoder_hidden] * beam_size
-            # Sum of log probability
-            sum_log_prob = [1.0] * beam_size
-            for di in range(max_length):
-                decoder_output, decoder_hidden, decoder_attention = decoder(
-                    decoder_input, decoder_hidden, encoder_outputs)
-                decoder_attentions[di] = decoder_attention.data
-                topv, topi = decoder_output.data.topk(1)
-                if topi.item() == EOS_token:
-                    decoded_words.append('<EOS>')
-                    break
-                else:
-                    decoded_words.append(output_lang.index2word[topi.item()])
-                decoder_input = topi.squeeze().detach()
-        return decoded_words, decoder_attentions[:di + 1]
-
+            beam_word_id_seqs = [[] for b in range(beam_size)]
+            beam_decoder_attentions = \
+                [torch.zeros(max_length, max_length) for b in range(beam_size)]
+            beam_sum_log_prob = \
+                [torch.zeros(1, device=device) for b in range(beam_size)]
+            # First step
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            beam_prev_hidden_states = \
+                [decoder_hidden.clone().detach() for b in range(beam_size)]
+            topv, topi = decoder_output.data.topk(beam_size)
+            vocab_size = decoder_output.shape[-1]
+            for b in range(beam_size):
+                beam_decoder_attentions[b][0] = decoder_attention.data
+                beam_word_id_seqs[b].append(topi[0, b].squeeze().detach())
+                beam_sum_log_prob[b] = beam_sum_log_prob[b] + topv[0, b].detach()
+            debug = False
+            if debug:
+                print('topv, topi:', topv, topi)
+                print('topi[0, 0]:', topi[0, 0])
+                print('beam_word_id_seqs:', beam_word_id_seqs)
+                print('beam_decoder_attentions:', beam_decoder_attentions)
+                print('beam_sum_log_prob:', beam_sum_log_prob)
+                raise NotImplementedError
+            for di in range(1, max_length):
+                debug = True
+                beam_decoder_outputs = []
+                for b in range(beam_size):
+                    decoder_output, decoder_hidden, decoder_attention = decoder(
+                        beam_word_id_seqs[b][-1], 
+                        beam_prev_hidden_states[b], 
+                        encoder_outputs)
+                    beam_decoder_attentions[b][di] = decoder_attention.data
+                    beam_prev_hidden_states[b] = decoder_hidden.clone().detach()
+                    beam_decoder_outputs.append(
+                        decoder_output.squeeze() + beam_sum_log_prob[b])
+                cat_outputs = torch.cat(beam_decoder_outputs, dim=0)
+                topv, topi = cat_outputs.data.topk(beam_size)
+                # Calculate the beam_id and word_id
+                top_beam_ids = [cat_id // vocab_size for cat_id in topi]
+                top_word_ids = [cat_id % vocab_size for cat_id in topi]
+                # Generate new beams
+                new_beam_word_id_seqs = []
+                new_beam_prev_hidden_states = []
+                new_beam_decoder_attentions = []
+                new_beam_sum_log_prob = []
+                for beam_id, word_id, sum_log_prob in \
+                    zip(top_beam_ids, top_word_ids, topv):
+                    b = beam_id.item()
+                    new_beam_word_id_seqs.append(
+                        deepcopy(beam_word_id_seqs[b]))
+                    new_beam_word_id_seqs[-1].append(word_id.detach())
+                    new_beam_prev_hidden_states.append(
+                        deepcopy(beam_prev_hidden_states[b]))
+                    new_beam_decoder_attentions.append(
+                        deepcopy(beam_decoder_attentions[b]))
+                    new_beam_sum_log_prob.append(
+                        sum_log_prob.detach().clone())
+                beam_word_id_seqs = new_beam_word_id_seqs
+                beam_decoder_attentions = new_beam_decoder_attentions
+                beam_sum_log_prob = new_beam_sum_log_prob
+                beam_prev_hidden_states = new_beam_prev_hidden_states
+                if top_word_ids[0].item() == EOS_token:
+                    for word_id in beam_word_id_seqs[0]:
+                        decoded_words.append(output_lang.index2word[word_id.item()])
+                    return decoded_words, beam_decoder_attentions[0][:di + 1]
+                if debug:
+                    print('cat_outputs:', cat_outputs.shape)
+                    print('topv, topi:', topv, topi)
+                    print('vocab_size:', vocab_size)
+                    print('top_beam_ids:', top_beam_ids)
+                    print('top_word_ids:', top_word_ids)
+                    print('new_beam_word_id_seqs:', new_beam_word_id_seqs)
+                    print('new_beam_prev_hidden_states:', new_beam_prev_hidden_states)
+                    print('new_beam_decoder_attentions:', new_beam_decoder_attentions)
+                    print('new_beam_sum_log_prob:', new_beam_sum_log_prob)
+                    #if di > 3:
+                    #    raise NotImplementedError
+                #for b in range(beam_size):
+                #    beam_word_id_seqs[b].append(topi[0, b].squeeze().detach())
+                
 
 def evaluateRandomly(encoder, decoder, pairs,
                      input_lang, output_lang, n=10,
@@ -110,13 +174,20 @@ def evaluateBLEU(encoder, decoder, pairs,
     encoder.eval()
     decoder.eval()
     scores = []
+    err_count = 0
     for i, pair in enumerate(pairs):
         gt_words = pair[1].split(' ')
-        output_words, attentions = evaluate(
-            encoder, decoder, pair[0],
-            input_lang, output_lang,
-            input_use_char=input_use_char,
-            beam_size=beam_size)
+        try:
+            output_words, attentions = evaluate(
+                encoder, decoder, pair[0],
+                input_lang, output_lang,
+                input_use_char=input_use_char,
+                beam_size=beam_size)
+        except:
+            print('pair[0]:', pair[0])
+            err_count = err_count + 1
+            break
+            continue
         output_words= output_words[0:-1]
         output_sentence = ' '.join(output_words)
         print('gt_words:', gt_words)
@@ -131,6 +202,7 @@ def evaluateBLEU(encoder, decoder, pairs,
             print('')
     avg_score = np.mean(scores)
     print('Test BLEU Score: {:.2f}'.format(avg_score))
+    print('err_count: {}'.format(err_count))
     return avg_score
  
 
